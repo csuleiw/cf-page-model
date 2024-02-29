6a3c8b673fc55f8cd23e3a0d221862bc755d4e52
csuleiw <103889883+csuleiw@users.noreply.github.com>
2024-02-29 16:09:03 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'enchanted-copper-ironclad.glitch.me'
+      url.hostname = 'motley-flashy-warbler.glitch.me'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
